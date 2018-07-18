1daa8b02fe1e6eeed57f871e5938d7bc2862216a
Gleb <zadachings@gmail.com>
2018-07-18 17:14:14 +0300

    added question controller

--- TestMakerFree/Controllers/QuestionController.cs
+++ TestMakerFree/Controllers/QuestionController.cs
@@ -1,8 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Mapster;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TestMakerFree.Data;
+using TestMakerFree.Data.Models;
 using TestMakerFree.ViewModels;
 
 namespace TestMakerFree.Controllers
@@ -10,6 +13,17 @@ namespace TestMakerFree.Controllers
     [Route("api/[controller]")]
     public class QuestionController : Controller
     {
+        #region Private Fields
+        private ApplicationDbContext _context;
+        #endregion
+
+        #region Constructor
+        public QuestionController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        #endregion
+
         #region RESTful conventions method
         /// <summary>
         /// Retrieves the question with the given {id}
@@ -19,7 +33,19 @@ namespace TestMakerFree.Controllers
         [HttpGet("{id}")]
         public IActionResult Get(int id)
         {
-            return Content("not implemented yet");
+            var question = _context.Questions.Where(i => i.Id == id).FirstOrDefault();
+
+            if (question == null)
+            {
+                return NotFound(new
+                {
+                    Error = String.Format("Question ID {0} has not been found", id)
+                });
+            }
+
+            return new JsonResult(question.Adapt<QuestionViewModel>(), new Newtonsoft.Json.JsonSerializerSettings(){
+                Formatting = Newtonsoft.Json.Formatting.Indented
+            });
         }
 
         /// <summary>
@@ -29,7 +55,22 @@ namespace TestMakerFree.Controllers
         [HttpPost]
         public IActionResult Post(QuestionViewModel vm)
         {
-            throw new NotImplementedException();
+            if (vm == null) return new StatusCodeResult(500);
+
+            var question = vm.Adapt<Question>();
+
+            question.QuizId = vm.QuizId;
+            question.Text = vm.Text;
+            question.Notes = vm.Notes;
+            question.CreatedDate = vm.CreatedDate;
+            question.LastModified = vm.CreatedDate;
+
+            _context.Questions.Add(question);
+            _context.SaveChanges();
+
+            return new JsonResult(question.Adapt<QuestionViewModel>(), new Newtonsoft.Json.JsonSerializerSettings(){
+                Formatting = Newtonsoft.Json.Formatting.Indented
+            });
         }
 
         /// <summary>
@@ -37,9 +78,29 @@ namespace TestMakerFree.Controllers
         /// </summary>
         /// <param name="vm">The question viewmodel containing the data to edit</param>
         [HttpPost]
-        public IActionResult Put(QuestionViewModel vm)
+        public IActionResult Put([FromBody]QuestionViewModel vm)
         {
-            throw new NotImplementedException();
+            if (vm == null) return new StatusCodeResult(500);
+
+            var question = _context.Questions.Where(i => i.Id == vm.Id).FirstOrDefault();
+
+            if (question == null)
+            {
+                return NotFound();
+            }
+
+            question.QuizId = vm.QuizId;
+            question.Text = vm.Text;
+            question.Notes = vm.Notes;
+            question.CreatedDate = vm.CreatedDate;
+            question.LastModified = vm.CreatedDate;
+
+            _context.SaveChanges();
+
+            return new JsonResult(question.Adapt<QuestionViewModel>(), new Newtonsoft.Json.JsonSerializerSettings()
+            {
+                Formatting = Newtonsoft.Json.Formatting.Indented
+            });
         }
 
         /// <summary>
@@ -49,37 +110,23 @@ namespace TestMakerFree.Controllers
         [HttpDelete("{id}")]
         public IActionResult Delete(int id)
         {
-            throw new NotImplementedException();
+            var question = _context.Questions.Where(i => i.Id == id).FirstOrDefault();
+
+            if (question == null)
+                return NotFound();
+            _context.Questions.Remove(question);
+            _context.SaveChanges();
+
+            return Ok();
         }
         #endregion
 
         [HttpGet("All/{quizId}")]
         public IActionResult All(int quizId)
         {
-            var sampleQuestions = new List<QuestionViewModel>();
+            var sampleQuestions = _context.Questions.Where(q => q.QuizId == quizId).ToArray();
 
-            sampleQuestions.Add(new QuestionViewModel()
-            {
-                Id = 1,
-                QuizId = quizId,
-                Text = "What do you value most in your life?",
-                CreatedDate = DateTime.Now,
-                LastModifiedDate = DateTime.Now
-            });
-
-            for (int i = 2; i <= 5; i++)
-            {
-                sampleQuestions.Add(new QuestionViewModel()
-                {
-                    Id = i,
-                    QuizId = quizId,
-                    Text = @"Sample question "+i,
-                    CreatedDate = DateTime.Now,
-                    LastModifiedDate = DateTime.Now
-                });
-            }
-
-            return new JsonResult(sampleQuestions,
+            return new JsonResult(sampleQuestions.Adapt<List<QuestionViewModel>>(),
                 new Newtonsoft.Json.JsonSerializerSettings()
                 {
                     Formatting = Newtonsoft.Json.Formatting.Indented
